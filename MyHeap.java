f3b2d66d592f84f24d4b5fd2b9de9fce1104667f
Jack Lam <jlam00@stuy.edu>
2019-05-01 21:45:34 -0400

    push down works

--- MyHeap.java
+++ MyHeap.java
@@ -1,17 +1,30 @@
+import java.util.*;
 public class MyHeap{
-  private static void pushDown(int[]data,int size,int index){
+  public static void pushDown(int[]data,int size,int index){
     if (2 * index + 1 >= size){
       return;
     }
+    int parent = data[index];
     int child1 = data[index * 2 + 1];
     int child2 = data[index * 2 + 2];
-    if (child1 > data[index]){
-      data[index * 2 + 1] = data[index];
+    if (child1 > data[index] && child1 >= child2){
+      data[index * 2 + 1] = parent;
       data[index] = child1;
+      pushDown(data,size, index * 2 +1);
     }
-    
+    else if(child2 > data[index] && child2 > child1){
+      data[index * 2 + 2] = parent;
+      data[index] = child2;
+      pushDown(data,size, index * 2 + 2);
+    }
   }
-  public static void heapify(int[]){
+  public static void heapify(int[] data){
 
+  }
+  public static void main(String[] args) {
+    int [] ary = new int[] {8,14,17,9,0,7,12};
+    System.out.println(Arrays.toString(ary));
+    pushDown(ary,7,0);
+    System.out.println(Arrays.toString(ary));
   }
 }
